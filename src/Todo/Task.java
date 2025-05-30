9aca1189263646668a32b255391bfb231cd69c61
sdawadi07 <Swastikadawadi5865@gmail.com>
2025-05-29 20:00:21 -0600

    Added constructor, getter and setter in the Task.java class

---
+++ src/Todo/Task.java
@@ -0,0 +1,41 @@
+package Todo;
+
+public class Task {
+    private int id;
+    private boolean done;
+     private String title;
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Task(int id) {
+        this.id = id;
+
+
+    }
+}
