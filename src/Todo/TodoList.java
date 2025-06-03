16470458f1ecb7bbafd15413b7e5a4c791c3496c
sdawadi07 <Swastikadawadi5865@gmail.com>
2025-06-02 22:04:29 -0600

    Added title, task id, and list of tasks that can be added in todolist.

--- src/Todo/TodoList.java
+++ src/Todo/TodoList.java
@@ -1,4 +1,36 @@
 package Todo;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class TodoList {
+    private List<Task> tasks;
+    private int nextId;
+    public TodoList() {
+        tasks = new ArrayList<>();
+        nextId =1;
+
+    }
+    public Task addTask (String title){
+        Task t = new Task(nextId,title);
+        nextId= nextId + 1;
+        tasks.add(t);
+        return t;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+    public boolean markDone (int Id){
+        for (Task t:tasks){
+            if(t.getId() == Id){
+             t.setDone(true);
+             return(true);
+            }
+
+        }
+        return false;
+    }
+
 }
